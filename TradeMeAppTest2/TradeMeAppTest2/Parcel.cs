ba60c3940fe934a232f7383fd83b86702eb06843
stephenjtd <stephen_doyle_2000@yahoo.com>
2018-03-01 19:07:54 +0000

    Add files via upload

---
+++ TradeMeAppTest2/TradeMeAppTest2/Parcel.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+public class Parcel
+{
+    private double weight;
+    private double length;
+    private double breadth;
+    private double height;
+    private String size;
+    private double cost;
+    private double smallcost = 5.00;
+    private double mediumcost = 7.50;
+    private double largecost = 8.50;
+
+    public Parcel(double weight, double length, double breadth, double height)
+    {
+        this.weight = weight;
+        this.length = length;
+        this.breadth = breadth;
+        this.height = height;
+        setSize();
+    }
+
+    public void setLength(double length)
+    {
+        this.length = length;
+    }
+
+    public void setBreadth(double breadth)
+    {
+        this.breadth = breadth;
+    }
+
+    public void setHeight(double height)
+    {
+        this.height = height;
+    }
+
+    public void setWeight(double weight)
+    {
+        this.weight = weight;
+    }
+
+    public void setSize()
+    {
+        if (weight > 25)
+            size = "over limit";
+        else
+        {
+            if ((length <= 200) || (breadth <= 300) || (height <= 150))
+                size = "small";
+            if ((length > 200) || (breadth > 300) || (height > 150))
+                size = "medium";
+            if ((length > 300) || (breadth > 400) || (height > 200))
+                size = "large";
+            if ((length > 400) || (breadth > 600) || (height > 250))
+                size = "over limit";
+        }
+
+       setCost();
+    }
+
+    public void setSizeCosts(double small, double medium, double large) {
+        smallcost = small;
+        mediumcost = medium;
+        largecost = large;
+    }
+
+    public void setCost()
+    {
+        if (size.Equals("small"))
+            cost = smallcost;
+        else if (size.Equals("medium"))
+            cost = mediumcost;
+        else if (size.Equals("large"))
+            cost = largecost;
+
+    }
+
+
+    public String getSize()
+    {
+        return size;
+    }
+
+    public double getCost()
+    {
+        return cost;
+    }
+
+    public double getLength()
+    {
+        return length;
+    }
+
+    public double getBreadth()
+    {
+        return breadth;
+    }
+
+    public double getHeight()
+    {
+        return height;
+    }
+
+    public double getWeight()
+    {
+        return cost;
+    }
+}
+
---
+++ TradeMeAppTest2/TradeMeAppTest2/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace TradeMeAppTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Program p = new Program();
+                Boolean keepGoing = true;
+                double[] sizes = new double[3];
+                Console.WriteLine("Parse the Parcel 2\n-----------------");
+
+                while (keepGoing)
+                {
+                    Console.WriteLine("Select an option:");
+
+                    Console.WriteLine("1. Calculate a parcel cost\n2. Change size cost values\n3. Exit");
+                    String s = Console.ReadLine();
+                    if (s.Equals("1"))
+                    {
+                        p.enterDetails(sizes);
+                    }
+                    else if (s.Equals("2"))
+                    {
+                        sizes = p.changeSize();
+                    }
+                    else if (s.Equals("3"))
+                    {
+                        Console.WriteLine("OK, program will now terminate");
+                        break;
+                    }
+                    else
+                        Console.WriteLine("Not a valid choice.  Try again.");
+
+                    Console.WriteLine("");
+
+                }
+                
+            }
+        }//main
+
+
+        void enterDetails(double[] sizes)
+        {
+            Console.WriteLine("Enter weight in kg (max 25kg):");
+            double weight = dataInput();
+            Console.WriteLine("Enter length in mm (max 400mm):");
+            double length = dataInput();
+            Console.WriteLine("Enter breadth in mm (max 600mm):");
+            double breadth = dataInput();
+            Console.WriteLine("Enter height in mm (max 250mm):");
+            double height = dataInput();
+
+            Parcel par = new Parcel(weight, length, breadth, height);
+            Console.WriteLine("Calculating size and cost...");
+
+            if (sizes != null)
+            {
+                par.setSizeCosts(sizes[0], sizes[1], sizes[2]);
+                par.setSize();
+            }
+
+            String size = par.getSize();
+            double cost = par.getCost();
+
+            if (size.Equals("over limit"))
+                Console.WriteLine("Package exceeds limits.  ");
+            else
+            {
+                Console.WriteLine("Type of package: " + size);
+                Console.WriteLine("Cost of package: $" + cost);
+            }
+        }
+
+        double[] changeSize()
+        {
+            double[] sizes = new double[3];
+            Console.WriteLine("Enter new cost for small package:");
+            sizes[0] = dataInput();
+            Console.WriteLine("Enter new cost for medium package:");
+            sizes[1] = dataInput();
+            Console.WriteLine("Enter new cost for large package:");
+            sizes[2] = dataInput();
+            return sizes;
+        }//changeSize
+
+        double dataInput()
+        {
+            double value = 0;
+            Boolean b = true;
+            while (b)
+            {
+                if (double.TryParse(Console.ReadLine(), out value))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid entry.  Please enter a number.");
+                }
+            }
+            return value;
+        }//dataInput
+
+    }
+
+}
+
